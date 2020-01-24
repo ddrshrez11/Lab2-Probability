c3c37fcf49dc4427e9d144a6e73498db0564a19a
Yunip Shrestha <yunipshrestha@gmail.com>
2020-01-24 20:45:05 +0545

    count words

---
+++ Word Count.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int wordcount()
+{
+    ifstream fin;
+    fin.open("lorem.txt");
+    string data;
+    long int Count=0;
+    while(fin.eof()!=1)
+    {
+        fin>>data;
+        //cout<<data[0];
+        if ((data[0]>=65 && data[0]<=90)||(data[0]>=97 && data[0]<=122))
+        {
+            Count++;
+        }
+
+    }
+    //cout<<Count;
+    return Count;
+}
+//807861
